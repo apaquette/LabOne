d0d305f7954c37e4312bbb06b8c919f3f4801348
Alex Paquette <alexandre.d.paquette@gmail.com>
2023-09-21 09:37:38 +0100

    Implement Semaphore Functionality

--- helloThreads.cpp
+++ helloThreads.cpp
@@ -10,12 +10,15 @@ void taskOne(std::shared_ptr<Semaphore> theSemaphore, int delay){
   std::cout << "must ";
   std::cout << "print ";
   std::cout << "first"<<std::endl;
+  theSemaphore->Signal();
   //tell taskTwo to start now
 }
 
 /*! displays a message second*/
 void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
   //wait here until taskOne finishes...
+  theSemaphore->Wait();
+
   std::cout <<"This ";
   std::cout << "will ";
   sleep(5);
